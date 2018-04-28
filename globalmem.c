d133393a1179f25ef9ea372517a4ceee249a9645
YanFuHai06 <2239243574@qq.com>
2018-04-28 09:33:41 +0800

    v0.1

---
+++ globalmem.c
@@ -0,0 +1,12 @@
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+
+#define GLOBALMEM_SIZE 0x1000
+#define MEM_CLEAR 0x1
+#define GLOBALMEM_MAJOR 230
+
+static int globalmem_major = GLOBALMEM_MAJOR;
+
